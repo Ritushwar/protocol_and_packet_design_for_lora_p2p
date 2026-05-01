24d61836ee1bcfd5594b8035aa9aa039656995d2
Ritushwar <ritushwarneupane111@gmail.com>
2026-05-01 20:27:19 +0545

    developing msg packet

---
+++ test/test_packet_2.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "serial_io.h"
+#include "lora_packet.h"
+#include "text_protocol.h"
+
+uint8_t u_id_s = 0x00;
+uint8_t n_id_s = 0x00;
+uint8_t u_id_r = 0x01;
+uint8_t n_id_r = 0x01;
+void setup(){
+    serialIO_init(9600);
+
+    set_my_add(u_id_s, n_id_s);
+    set_r_add(u_id_r, n_id_r);
+    Serial.println("Type something to send and Enter:");
+}
+
+void loop(){
+    if(serialIO_available()){
+        std:: string msg = serialIO_read();
+
+        int msg_len = msg.length();
+        serialIO_print("Received text: " + msg);
+        Serial.println("Msg length: " + String(msg_len));
+        develop_msg_packet(msg);
+    }
+}
